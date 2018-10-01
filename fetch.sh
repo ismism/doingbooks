2826dbb01a57ac5e4bc0de9076bd60b813ee9b1a
femilamarr <femilamarr@gmail.com>
2018-10-01 17:41:04 +0900

    책 정보 갱신

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl -s "https://docs.google.com/spreadsheets/d/1Gojf8ZFqVC5ubdU9ltdU7CpTc8mwbZcnA2CV-MthOJE/export?format=csv&id=1Gojf8ZFqVC5ubdU9ltdU7CpTc8mwbZcnA2CV-MthOJE&gid=0" > _data/books.csv
+curl -s "https://docs.google.com/spreadsheets/d/1gGnSWbjrusMygrzlpJjfPSqBHl-wW8FL3azIxdi7gx8/export?format=csv&id=1gGnSWbjrusMygrzlpJjfPSqBHl-wW8FL3azIxdi7gx8&gid=0" > _data/books.csv
 
