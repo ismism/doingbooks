3a3235549de3518789c4560df7a6b97016894e48
femilamarr <femilamarr@gmail.com>
2020-03-24 23:36:22 +0900

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-curl -s "https://docs.google.com/spreadsheets/d/1gGnSWbjrusMygrzlpJjfPSqBHl-wW8FL3azIxdi7gx8/export?format=csv&id=1gGnSWbjrusMygrzlpJjfPSqBHl-wW8FL3azIxdi7gx8&gid=0" > _data/books.csv
-
+curl -s 'https://docs.google.com/spreadsheets/u/3/d/1w5f__CqDoKQX3dKJbwGMccbRw0pmon_uI85XLlT7w6A/export?format=csv&id=1w5f__CqDoKQX3dKJbwGMccbRw0pmon_uI85XLlT7w6A&gid=321920119' > _data/books.csv
