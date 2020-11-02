e489356003f86b2ece9bb21a7a8c003f3d026e46
femilamarr <femilamarr@gmail.com>
2020-11-02 19:47:39 +0900

    fix: add -L option to curl to follow redirect

--- fetch.sh
+++ fetch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -s 'https://docs.google.com/spreadsheets/u/3/d/1w5f__CqDoKQX3dKJbwGMccbRw0pmon_uI85XLlT7w6A/export?format=csv&id=1w5f__CqDoKQX3dKJbwGMccbRw0pmon_uI85XLlT7w6A&gid=321920119' > _data/books.csv
+curl -sL 'https://docs.google.com/spreadsheets/d/1w5f__CqDoKQX3dKJbwGMccbRw0pmon_uI85XLlT7w6A/export?format=csv&gid=321920119' > _data/books.csv
